47dde139b880f9abbb59b38e63f2c6bfdf8a7a47
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-27 23:39:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,9 @@ class Triangle
   
   def kind
     @sides.uniq!
-    if sides.length == 1
+    if @s1 <= @s2 + @s3 || @s2 <= @s1 + @s3 || @s3 <= @s1 + @s2
+      raise TriangleError
+    elsif sides.length == 1
       self.kind = :equilateral 
     elsif sides.length == 2 
       self.kind = :isosceles
